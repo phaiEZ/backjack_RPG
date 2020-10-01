907a728fcf5218ad15bb50096770f940b01e7c4d
CHAYUT PHULPHET <63010200@KMITL.AC.TH>
2020-10-01 10:04:45 +0700

    10/1/2020

--- backjack_RPG/Source.cpp
+++ backjack_RPG/Source.cpp
@@ -7,36 +7,52 @@ int main()
 
 	////// Texture
 	sf::Texture playerTexture;
-	if (!playerTexture.loadFromFile("achievements.png"))
+	if (!playerTexture.loadFromFile("new2.png"))
 	{
 		std::cout << "Load failed" << std::endl;
 	}
-
 	////// Sprite
 	sf::Sprite shapeSprite;
 	shapeSprite.setTexture(playerTexture);
 	float spriteX = playerTexture.getSize().x / 32.00000;
 	float spriteY = playerTexture.getSize().y / 32.00000;
 	shapeSprite.setTextureRect(sf::IntRect(0, 0, spriteX, spriteY));
-	sf::Vector2f spawnPoint = { 0.f, 0.f };
+	sf::Vector2f spawnPoint = { 450- spriteY , 450 - spriteX  };
 	shapeSprite.setPosition(spawnPoint);
 
+	/// wallpaper
+	sf::Texture wallpaper_;
+	if (!playerTexture.loadFromFile("new2.png"))
+	{
+		std::cout << "Load failed" << std::endl;
+	}
+	sf::Sprite wallpaper;
+	wallpaper.setTexture(wallpaper_);
 	while (window.isOpen())
 	{
+		window.draw(shapeSprite);
 		window.draw(shapeSprite);
 		shapeSprite.setTextureRect(sf::IntRect(spriteX * 31.00000, spriteY * 0.00000, spriteX, spriteY));
 		window.display();
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-			shapeSprite.move(.1f, 0.f);
+			shapeSprite.move(50, 0);
+			while (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-			shapeSprite.move(-.1f, 0.f);
+			shapeSprite.move(-50, 0);
+			while (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-			shapeSprite.move(0.f, .1f);
+			shapeSprite.move(0, 50);
+			while (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-			shapeSprite.move(0.f, -.1f);
+			shapeSprite.move(0, -50);
+			while (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 		{
